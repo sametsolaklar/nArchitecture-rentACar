4732ca9538f29e7755342eaaf9a8022dd07207e2
Samet <Samet@DESKTOP-KJFE73I>
2023-06-04 16:28:16 +0300

    Add CQRS

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+             IBrandRepository _brandRepository;
+             IMapper _mapper;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+            }
+
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand mappedBrand = _mapper.Map<Brand>(request);
+                Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+                return createdBrandDto;
+            }
+        }
+    }
+}
--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
